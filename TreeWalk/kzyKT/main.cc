5c991df1cd62469fe37cfa47412132eccbb3abee
kzyKT <s1200008aizu@gmail.com>
2016-02-23 00:19:30 +0900

    update code

--- TreeWalk/kzyKT/main.cc
+++ TreeWalk/kzyKT/main.cc
@@ -13,25 +13,20 @@ void dfs(int x,int k) {
 }
 
 bool check(int s, int t, int n) {
-  if(d[s]==0&&d[t]==n/2) return 1;
-  if(d[s]==0&&d[t]==n-1) return 1;
-  if(d[s]==n/2-1&&d[t]==n/2) return 1;
-  if(d[s]==n/2-1&&d[t]==n-1) return 1;
-  return 0;
+  return !d[s]&&(d[t]==n/2||d[t]==n-1)||d[s]==n/2-1&&(d[t]==n/2||d[t]==n-1);
 }
 
 int main() {
   int n,s,t;
   cin >> n >> s >> t;
-  for(int i=0; i<n-1; i++) {
-    int x,y;
+  for(int i=0,x,y; i<n-1; i++) {
     cin >> x >> y;
     v[x].push_back(y);
     v[y].push_back(x);
   }
   memset(d,-1,sizeof(d));
   dfs(1,0);
-  int M=-1,x;
+  int M=-1,x,f=0;
   for(int i=1; i<=n; i++) {
     if(d[i]>M) {
       M=d[i];
@@ -40,7 +35,6 @@ int main() {
   }
   memset(d,-1,sizeof(d));
   dfs(x,0);
-  bool f=0;
   for(int i=0; i<=n; i++) f|=d[i]==n-1;
   if(f&&(check(s,t,n)||check(t,s,n))) cout << "Yes" << endl;
   else cout << "No" << endl;
