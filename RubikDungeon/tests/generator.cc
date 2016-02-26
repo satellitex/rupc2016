4300efd7d64045741f275c7d754c68563b39bb3e
kzyKT <s1200008aizu@gmail.com>
2016-02-26 17:51:27 +0900

    add testcase

--- RubikDungeon/tests/generator.cc
+++ RubikDungeon/tests/generator.cc
@@ -3,11 +3,13 @@
 using namespace std;
 
 int main(int argc, char* argv[]) {
-  registerGen(argc, argv);
-  for(int t=0; t<10; t++) {
-    ofstream ofs(format("03_random_%02d.in",t).c_str());
-    //int n=rnd.next(2,10);
-    int n=rnd.next(2,100000);
+  registerGen(argc, argv,1);
+  for(int t=0; t<20; t++) {
+    //ofstream ofs(format("03_random_%02d.in",t).c_str());
+    ofstream ofs(format("05_random_%02d.in",t).c_str());
+    int n;
+    if(t<10) n=rnd.next(2,10);
+    else n=rnd.next(2,100000);
     ofs << n << endl;
     int x=rnd.next(0,n-1),y=rnd.next(0,n-1),z=rnd.next(0,n-1);
     ofs << x << " " << y << " " << z << endl;
