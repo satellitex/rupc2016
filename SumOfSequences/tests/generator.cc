20fa8fc3998198e4c41fc558dd4d9244aa9d13ff
kzyKT <s1200008aizu@gmail.com>
2016-02-26 16:32:26 +0900

    add testcase

--- SumOfSequences/tests/generator.cc
+++ SumOfSequences/tests/generator.cc
@@ -5,7 +5,8 @@ using namespace std;
 int main(int argc, char* argv[]) {
   registerGen(argc, argv, 1);
   for(int t=0; t<10; t++) {
-    ofstream ofs(format("03_random_%02d.in",t).c_str());
+    //ofstream ofs(format("03_random_%02d.in",t).c_str());
+    ofstream ofs(format("04_random_%02d.in",t).c_str());
     int n=rnd.next(1,40000),m=rnd.next(1,40000),q=rnd.next(1,100000);
     ofs << n << " " << m << " " << q << endl;
     for(int i=0; i<n; i++) {
