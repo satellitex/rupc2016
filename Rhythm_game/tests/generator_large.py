a21586bc492f857b762805dbebab2eeffab06c1b
RollMan <qwpmb554@gmail.com>
2016-02-27 18:12:21 +0900

    add testcases

---
+++ Rhythm_game/tests/generator_large.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import random
+
+NUMOFCASE = 10
+N_MIN = 1
+N_MAX = 1000
+M_MIN = 0
+M_MAX = 100
+
+seq = 0
+
+def generate():
+  global seq
+
+  n = N_MAX
+  m = M_MAX
+  print("{} {}".format(n, m))
+  for i in range(m):
+    a = random.randint(N_MIN, n)
+    b = random.randint(N_MIN, n)
+    while a == b:
+      b = random.randint(N_MIN, n)
+    print("{} {}".format(a, b))
+
+def main():
+  global seq
+
+  generate()
+
+if __name__ == '__main__':
+  main()
